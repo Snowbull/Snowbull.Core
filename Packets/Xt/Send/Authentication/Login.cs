a977970275d03518b010a7b074a8b1fa3066f7d0
Static <staticabc@live.co.uk>
2016-02-20 12:21:22 +0000

    Added authentication success packet.

---
+++ Packets/Xt/Send/Authentication/Login.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Snowbull.API.Packets.Xt.Send.Authentication {
+    public class Login : XtPacket, ISendPacket {
+        public Login(int id, string key, string populations) : base(new XtData(From.Server, "l", new string[] { id.ToString(), key, "", populations })) {
+        }
+
+        public Login() : base(new XtData(From.Server, "l")) {
+        }
+    }
+}
+
