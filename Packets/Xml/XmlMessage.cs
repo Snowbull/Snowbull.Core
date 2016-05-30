7a7cf5336fa93ecea0cb32b65e26082cf0e97c95
Static <staticabc@live.co.uk>
2016-05-30 15:52:42 +0100

    Added copyright headers to source code files.

--- Packets/Xml/XmlMessage.cs
+++ Packets/Xml/XmlMessage.cs
@@ -1,8 +1,31 @@
-﻿using System;
+﻿/**
+ * Base XML Message for Snowbull's Plugin API ("Snowbull.API").
+ *
+ * Copyright 2016 by Lewis Hazell <staticabc@live.co.uk>
+ *
+ * This file is part of "Snowbull.API".
+ * 
+ * "Snowbull.API" is free software: you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation, either version
+ * 3 of the License, or (at your option) any later version.
+ * 
+ * "Snowbull.API" is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty 
+ * of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See
+ * the GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License
+ * along with "Snowbull.API". If not, see <http://www.gnu.org/licenses/>.
+ *
+ * License: GPL-3.0 <https://www.gnu.org/licenses/gpl-3.0.txt>
+ */
+
+using System;
 using System.Xml;
 
 namespace Snowbull.API.Packets.Xml {
-    public class XmlMessage  : XmlPacket {
+    public abstract class XmlMessage  : XmlPacket {
         public string Action {
             get;
             private set;
@@ -13,7 +36,7 @@ namespace Snowbull.API.Packets.Xml {
             private set;
         }
 
-        public XmlMessage(XmlDocument doc) : base(doc) {
+        internal XmlMessage(XmlDocument doc) : base(doc) {
             XmlElement xml = doc.DocumentElement;
             Body = null;
             if(!xml.HasChildNodes) throw new MessageParseException("Xml data has no child nodes to read.");
@@ -23,12 +46,12 @@ namespace Snowbull.API.Packets.Xml {
             Action = Body.Attributes["action"].Value;
         }
 
-        public XmlMessage(XmlDocument doc, XmlNode body) : base(doc) {
+        internal XmlMessage(XmlDocument doc, XmlNode body) : base(doc) {
             Body  = body;
             Action = Body.Attributes["action"].Value;
         }
 
-        public XmlMessage(XmlDocument doc, string action, int r, XmlNode[] inner = null) : base(Create(doc, action, r, inner)) {
+        internal XmlMessage(XmlDocument doc, string action, int r, XmlNode[] inner = null) : base(Create(doc, action, r, inner)) {
 
         }
 
