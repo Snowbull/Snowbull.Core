bead6a7b80d3bfa127aa22eef7bb8c2e817612a9
Static <staticabc@live.co.uk>
2016-05-31 03:24:40 +0100

    Added configuration section handler for Snowbull's configuration. TODO: Configuration reading.

---
+++ Snowbull/Configuration/Server.cs
@@ -0,0 +1,58 @@
+﻿/**
+ * Server Configuration for Snowbull.
+ *
+ * Copyright 2016 by Lewis Hazell <staticabc@live.co.uk>
+ *
+ * This file is part of Snowbull.
+ * 
+ * Snowbull is free software: you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation, either version
+ * 3 of the License, or (at your option) any later version.
+ * 
+ * Snowbull is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty 
+ * of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See
+ * the GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License
+ * along with Snowbull. If not, see <http://www.gnu.org/licenses/>.
+ *
+ * License: GPL-3.0 <https://www.gnu.org/licenses/gpl-3.0.txt>
+ */
+
+using System;
+using System.Configuration;
+
+namespace Snowbull {
+	public class Server {
+		[ConfigurationProperty("id", IsRequired=true)]
+		public int Id {
+			get {
+				return int.Parse(this["id"] as string);
+			}
+		}
+
+		[ConfigurationProperty("name", IsRequired=true)]
+		public string Name {
+			get {
+				return this["name"] as string;
+			}
+		}
+
+		[ConfigurationProperty("port", IsRequired=true)]
+		public int Port {
+			get {
+				return int.Parse(this["port"] as string);
+			}
+		}
+
+		[ConfigurationProperty("type", IsRequired=true)]
+		public Type Type {
+			get {
+				return Type.GetType(this["type"] as string);
+			}
+		}
+	}
+}
+
---
+++ Snowbull/Configuration/SnowbullConfigurationSection.cs
@@ -0,0 +1,65 @@
+﻿/**
+ * Configuration Section for Snowbull.
+ *
+ * Copyright 2016 by Lewis Hazell <staticabc@live.co.uk>
+ *
+ * This file is part of Snowbull.
+ * 
+ * Snowbull is free software: you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation, either version
+ * 3 of the License, or (at your option) any later version.
+ * 
+ * Snowbull is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty 
+ * of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See
+ * the GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License
+ * along with Snowbull. If not, see <http://www.gnu.org/licenses/>.
+ *
+ * License: GPL-3.0 <https://www.gnu.org/licenses/gpl-3.0.txt>
+ */
+
+/**
+ * Example
+ *	<snowbull>
+ *		<servers>
+ *			<server id="1" name="Login" port="6112" type="Snowbull.Login.LoginServer" />
+ *			<server id="100" name="The Bull" port="9875" type="Snowbull.Game.GameServer" />
+ *		</servers>
+ *
+ *		<zones>
+ *			<zone id="1" name="w1" type="Snowbull.Game.GameZone" server="100" />
+ *			<zone id="2" name="w1" type="Snowbull.Login.LoginZone" server="1" />
+ *		</zones>
+ *	</snowbull>
+ */
+
+using System;
+using System.Configuration;
+
+namespace Snowbull.Configuration {
+	public class SnowbullConfigurationSection : ConfigurationSection {
+		public static SnowbullConfigurationSection GetConfiguration() {
+			return (SnowbullConfigurationSection) ConfigurationManager.GetSection("snowbull") ?? new SnowbullConfigurationSection();
+		}
+
+		[ConfigurationProperty("zones")]
+		[ConfigurationCollection(typeof(Zones), AddItemName = "zone")]
+		public Zones Zones {
+			get {
+				return this["zones"] as Zones;
+			}
+		}
+
+		[ConfigurationProperty("servers")]
+		[ConfigurationCollection(typeof(Servers), AddItemName = "server")]
+		public Servers Servers {
+			get {
+				return this["servers"] as Servers;
+			}
+		}
+	}
+}
+
