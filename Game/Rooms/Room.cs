e10197e2a9d7c486c2180ad4e47d15c996324604
StaticCP <staticabc@live.co.uk>
2016-10-19 19:09:25 +0100

    Added ActorRef property to IContext (should have been there anyway).

--- Game/Rooms/Room.cs
+++ Game/Rooms/Room.cs
@@ -1,7 +1,13 @@
 ﻿using System;
+using Akka.Actor;
 
 namespace Snowbull.Core {
     public class Room : IRoom {
+        public IActorRef ActorRef {
+            get;
+            protected set;
+        }
+
         public Room() {
         }
     }
--- IContext.cs
+++ IContext.cs
@@ -1,8 +1,10 @@
 ﻿using System;
-
+using Akka.Actor;
 namespace Snowbull.Core {
     public interface IContext {
-
+        IActorRef ActorRef {
+            get;
+        }
     }
 }
 
